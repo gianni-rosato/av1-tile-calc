a6781c1acf9c2631c5e14851fc4426c821e80400
Gianni <giannirosato@proton.me>
2023-11-27 17:35:43 -0500

    feat: AV1 Tile Calculator
    Also edited README

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# av1-tile-calc
-Command-line tool in Zig that uses resolution to calculate optimal tile dimensions for AV1 video encoding.
+# AV1 Tile Calculator
+
+CLI tool written in Zig that calculates the optimal tile dimensions for encoding video using the AV1 codec. It takes four command-line arguments:
+
+* width: the input width in pixels
+* height: the input height in pixels
+* fps: the input frames per second
+* bitrate_range: the desired bitrate range (accepts values 1, 2, or 3, corresponding to lowest, low, medium/high)
+
+The tool calculates the number of tiles required to achieve the desired bitrate range, and outputs the following information:
+
+* Tile Columns: the number of columns required to achieve the desired bitrate range, in both log-2 and literal formats.
+* Tile Rows: the number of rows required to achieve the desired bitrate range, in both log-2 and literal formats.
+* X-splits: the optimal Av1an x-splits value based on FPS. *useful for Av1an*
+* Lag in Frames: the optimal lag in frames value based on the desired bitrate range. *useful for popular aomenc forks*
+
+The output is generally useful for aomenc, SVT-AV1, libaom-av1 via FFmpeg, and rav1e, with some differences between how they may be used for each encoder. See the Help menu of the AV1 Tile Calculator for more info.
+
+```bash
+$ av1-tile-calc -h
+AV1 Tile Calculator | inspired by autocompressor.net/tools/av1-calculator
+Usage: av1-tile-calc [width] [height] [fps] [bitrate_range]
+
+Options:
+	Width:		Your input width in pixels
+	Height:		Your input height in pixels
+	fps:		Your input frames per second
+	Bitrate Range:	Accepts either lowest (1), low (2), medium/high (3)
+
+Output:
+	Log-2:		Useful for aomenc & SVT-AV1
+	Literal:	Useful for libaom-av1 & rav1e
+	X-split length:	The -x (--extra-splits) option in Av1an
+	Lag in Frames:	Useful for aom-av1-lavish & other forks
+```
+
+## Usage
+
+To use the tool, simply run the executable with the four command-line arguments:
+```bash
+$ av1-tile-calc [width] [height] [fps] [bitrate_range]
+```
+For example:
+```bash
+$ av1-tile-calc 1920 1080 24 3
+Bitrate Range: Medium/High
+Tile Columns:
+	Log-2:   0
+	Literal: 1
+Tile Rows:
+	Log-2:   0
+	Literal: 1
+X-splits:	 240
+Lag in Frames:	 48
+```
+This would calculate the tile dimensions for a 1920x1080 video at 24 fps, with a bitrate range of 3 (medium/high).
+
+## Compilation
+
+To build the tool, you will need to have the Zig programming language installed. Simply run the following command to build the executable:
+```
+zig build-exe av1-tile-calc.zig
+```
+This will generate an executable file called `av1-tile-calc` that can be run on your system.
+
+## License & Contributions
+
+The AV1 Tile Calculator was developed by Gianni Rosato and is licensed under the MIT License. See the included LICENSE file for more information.
+
+Special thank you to RootAtKali of Auto-Rez Media Technologies, LLC for the inspiration from their [AV1 Encoding Calculator](https://autocompressor.net/tools/av1-calculator) web tool.
---
+++ av1-tile-calc.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// This tool was inspired by https://autocompressor.net/tools/av1-calculator
+
+pub fn main() !void {
+    // Get allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Parse args into string array (error union needs 'try')
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    // hel is true if the user provides `-h` as their first cli argument
+    const hel: bool = std.mem.eql(u8, args[1], "-h");
+
+    // too many args
+    if (args.len > 5) {
+        print("Please provide at most 4 arguments.\n", .{});
+        print("Run `av1-tile-calc -h` for more info.\n", .{});
+        return;
+    }
+    // too few args
+    if (args.len < 5) {
+        if (hel) { // if the user provided `-h` ...
+            _ = help(); // run the help function to print the help menu
+            return;
+        } else {
+            print("Please provide at least 4 arguments.\n", .{});
+            print("Run `av1-tile-calc -h` for more info.\n", .{});
+            return;
+        }
+    }
+
+    // user-provided bitrate range. maps to lowest, low, medium/high
+    const bitrate_range: usize = try std.fmt.parseInt(usize, args[4], 10);
+
+    // calculate x-splits in Av1an based on user-provided fps. multiply it by 60 so it is easier to divide later
+    var xs: usize = try std.fmt.parseInt(usize, args[3], 10);
+    xs *= 60;
+
+    // initialize the lag in frames variable
+    var lif: usize = undefined;
+
+    switch (bitrate_range) {
+        1 => { // "lowest" bitrate target
+            print("Bitrate Range: Lowest\n", .{});
+            xs /= 3;
+            lif = 64;
+        },
+        2 => { // "low" bitrate target
+            print("Bitrate Range: Low\n", .{});
+            xs /= 4;
+            lif = 48;
+        },
+        3 => { // "medium/high" bitrate target
+            print("Bitrate Range: Medium/High\n", .{});
+            xs /= 6;
+            lif = 48;
+        },
+        else => { // Anything else will assume medium/high
+            print("Bitrate Range: Unknown, assuming Medium/High\n", .{});
+            xs /= 6;
+            lif = 48;
+        },
+    }
+
+    // Parse target width & height from the first & second args
+    var width: usize = try std.fmt.parseInt(usize, args[1], 10);
+    var height: usize = try std.fmt.parseInt(usize, args[2], 10);
+
+    // Target pixels per tile (actual result will be ≥2/3 and <4/3 of this number)
+    var tpx: usize = 2000000;
+
+    // initialize rows log 2 & columns log 2
+    var rowsl: usize = 0;
+    var colsl: usize = 0;
+
+    var ctpx: usize = width * height; // current tile pixels, starts at the full size of the video, we subdivide until <4/3 of tpx
+    var ctar: usize = width / height; // current tile aspect ratio, we subdivide into cols if >1 and rows if ≤1
+
+    _ = try getTiles(tpx, &rowsl, &colsl, &ctpx, &ctar);
+
+    // initialize literal columns & literal rows
+    const cols: usize = std.math.pow(usize, 2, colsl);
+    const rows: usize = std.math.pow(usize, 2, rowsl);
+
+    // print results
+    print("Tile Columns:\n", .{});
+    print("\tLog-2:   {d}\n", .{colsl});
+    print("\tLiteral: {d}\n", .{cols});
+
+    print("Tile Rows:\n", .{});
+    print("\tLog-2:   {d}\n", .{rowsl});
+    print("\tLiteral: {d}\n", .{rows});
+
+    print("X-splits:\t {d}\n", .{xs});
+    print("Lag in Frames:\t {d}\n", .{lif});
+}
+
+pub fn getTiles(tpx: usize, rowsl_ptr: *usize, colsl_ptr: *usize, ctpx_ptr: *usize, ctar_ptx: *usize) !void {
+    // tpx = 2,000,000 results in 1 tile at 1080p, tpx = 1,000,000 results in 2 tiles at 1080p
+
+    // while current tile pixels >= pixels per tile * 4/3
+    while (ctpx_ptr.* >= tpx * 4 / 3) {
+        if (ctar_ptx.* > 1) {
+            // Subdivide into columns, add 1 to colsl and halve ctar, then halve ctpx
+            colsl_ptr.* += 1;
+            ctar_ptx.* /= 2;
+            ctpx_ptr.* /= 2;
+        } else {
+            // Subdivide into rows, add 1 to rowsl and double ctar, then halve ctpx
+            rowsl_ptr.* += 1;
+            ctar_ptx.* *= 2;
+            ctpx_ptr.* /= 2;
+        }
+    }
+}
+
+pub fn help() void {
+    // Contains print statements for the help menu
+    print("AV1 Tile Calculator | inspired by autocompressor.net/tools/av1-calculator\n", .{});
+    print("Usage: av1-tile-calc [width] [height] [fps] [bitrate_range]\n", .{});
+    print("\n", .{});
+    print("Options:\n", .{});
+    print("\tWidth:\t\tYour input width in pixels\n", .{});
+    print("\tHeight:\t\tYour input height in pixels\n", .{});
+    print("\tfps:\t\tYour input frames per second\n", .{});
+    print("\tBitrate Range:\tAccepts either lowest (1), low (2), medium/high (3)\n", .{});
+    print("\n", .{});
+    print("Output:\n", .{});
+    print("\tLog-2:", .{});
+    print("\t\tUseful for aomenc & SVT-AV1\n", .{});
+    print("\tLiteral:", .{});
+    print("\tUseful for libaom-av1 & rav1e\n", .{});
+    print("\tX-split length:", .{});
+    print("\tThe -x (--extra-splits) option in Av1an\n", .{});
+    print("\tLag in Frames:", .{});
+    print("\tUseful for aom-av1-lavish & other forks\n", .{});
+    return;
+}
