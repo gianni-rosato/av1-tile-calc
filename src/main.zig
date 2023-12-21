6dd6f2cba57c9c018f1650c07d106cb5e5b790b6
Gianni <giannirosato@proton.me>
2023-12-21 14:49:58 -0500

    refactor: provide string for bitrate range
    You can now provide a string to specify the bitrate range instead of a number, and I've added a test (really just to test out Zig's test feature)

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
+const testing = std.testing;
+const parseInt = std.fmt.parseInt;
 
 // This tool was inspired by https://autocompressor.net/tools/av1-calculator
 
@@ -12,6 +14,15 @@ pub fn main() !void {
     // Parse args into string array (error union needs 'try')
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
+
+    const NegError = error{IsNegative};
+
+    const bitrateTarget = enum(u8) {
+        lowest = 1,
+        low = 2,
+        med = 3,
+        high = 4,
+    };
 
     if (args.len == 1) { // if the user provided no args ...
         print("Please provide at least 4 arguments.\n", .{});
@@ -26,13 +37,13 @@ pub fn main() !void {
 
     // check if the user provided a negative integer
     var isNeg: i16 = undefined;
-    for (args[1..]) |arg| { // for every argument ...
-        isNeg = try std.fmt.parseInt(i16, arg, 10); // parse each argument as an i16
+    for (args[1..4]) |arg| { // for every argument ...
+        isNeg = try parseInt(i16, arg, 10); // parse each argument as an i16
         // if the argument is less than 0 (it is negative) ...
         if (isNeg < 0) {
             print("Please provide positive integers.\n", .{});
             print("Run `av1-tile-calc -h` for more info.\n", .{});
-            return;
+            return NegError.IsNegative;
         }
     }
 
@@ -50,45 +61,59 @@ pub fn main() !void {
         return;
     }
 
-    // user-provided bitrate range. maps to lowest, low, medium/high
-    const bitrate_range: usize = try std.fmt.parseInt(usize, args[4], 10);
+    // user-provided bitrate range. maps to lowest, low, medium, and high
+    var bitrate_range: bitrateTarget = undefined;
+
+    if (std.mem.eql(u8, args[4], "lowest")) {
+        bitrate_range = bitrateTarget.lowest;
+    } else if (std.mem.eql(u8, args[4], "low")) {
+        bitrate_range = bitrateTarget.low;
+    } else if (std.mem.eql(u8, args[4], "med")) {
+        bitrate_range = bitrateTarget.med;
+    } else if (std.mem.eql(u8, args[4], "high")) {
+        bitrate_range = bitrateTarget.high;
+    } else {
+        print("Please provide a proper bitrate target argument.\n", .{});
+        return;
+    }
 
     // calculate x-splits in Av1an based on user-provided fps. multiply it by 60 so it is easier to divide later
-    var xs: usize = try std.fmt.parseInt(usize, args[3], 10);
+    var xs: usize = try parseInt(usize, args[3], 10);
     xs *= 60;
 
     // initialize the lag in frames variable
     var lif: usize = undefined;
 
+    // set the x-splits & lag in frames based on the bitrate range
     switch (bitrate_range) {
-        1 => { // "lowest" bitrate target
+        bitrateTarget.lowest => { // "lowest" bitrate target
             print("Bitrate Range: Lowest\n", .{});
             xs /= 3;
             lif = 64;
         },
-        2 => { // "low" bitrate target
+        bitrateTarget.low => { // "low" bitrate target
             print("Bitrate Range: Low\n", .{});
             xs /= 4;
             lif = 48;
         },
-        3 => { // "medium/high" bitrate target
-            print("Bitrate Range: Medium/High\n", .{});
+        bitrateTarget.med => { // "medium" bitrate target
+            print("Bitrate Range: Medium\n", .{});
             xs /= 6;
             lif = 48;
         },
-        else => { // Anything else will assume medium/high
-            print("Bitrate Range: Unknown, assuming Medium/High\n", .{});
+        bitrateTarget.high => { // "high" bitrate target
+            print("Bitrate Range: High\n", .{});
             xs /= 6;
             lif = 48;
         },
     }
 
     // Parse target width & height from the first & second args
-    var width: usize = try std.fmt.parseInt(usize, args[1], 10);
-    var height: usize = try std.fmt.parseInt(usize, args[2], 10);
+    const width: usize = try parseInt(usize, args[1], 10);
+    const height: usize = try parseInt(usize, args[2], 10);
 
     // Target pixels per tile (actual result will be ≥2/3 and <4/3 of this number)
-    var tpx: usize = 2000000;
+    const tpx: usize = 2000000;
 
     // initialize rows log 2 & columns log 2
     var rowsl: usize = 0;
@@ -97,7 +122,7 @@ pub fn main() !void {
     var ctpx: usize = width * height; // current tile pixels, starts at the full size of the video, we subdivide until <4/3 of tpx
     var ctar: usize = width / height; // current tile aspect ratio, we subdivide into cols if >1 and rows if ≤1
 
-    _ = try getTiles(tpx, &rowsl, &colsl, &ctpx, &ctar);
+    _ = getTiles(tpx, &rowsl, &colsl, &ctpx, &ctar);
 
     // initialize literal columns & literal rows
     const cols: usize = std.math.pow(usize, 2, colsl);
@@ -116,7 +141,7 @@ pub fn main() !void {
     print("Lag in Frames:\t {d}\n", .{lif});
 }
 
-pub fn getTiles(tpx: usize, rowsl_ptr: *usize, colsl_ptr: *usize, ctpx_ptr: *usize, ctar_ptx: *usize) !void {
+fn getTiles(tpx: usize, rowsl_ptr: *usize, colsl_ptr: *usize, ctpx_ptr: *usize, ctar_ptx: *usize) void {
     // tpx = 2,000,000 results in 1 tile at 1080p, tpx = 1,000,000 results in 2 tiles at 1080p
 
     // while current tile pixels >= pixels per tile * 4/3
@@ -135,7 +160,7 @@ pub fn getTiles(tpx: usize, rowsl_ptr: *usize, colsl_ptr: *usize, ctpx_ptr: *usi
     }
 }
 
-pub fn help() void {
+fn help() void {
     // Contains print statements for the help menu
     print("AV1 Tile Calculator | inspired by autocompressor.net/tools/av1-calculator\n", .{});
     print("Usage: av1-tile-calc [width] [height] [fps] [bitrate_range]\n", .{});
@@ -144,7 +169,7 @@ pub fn help() void {
     print("\tWidth:\t\tYour input width in pixels\n", .{});
     print("\tHeight:\t\tYour input height in pixels\n", .{});
     print("\tfps:\t\tYour input frames per second\n", .{});
-    print("\tBitrate Range:\tAccepts either lowest (1), low (2), medium/high (3)\n", .{});
+    print("\tBitrate Range:\tAccepts `lowest`, `low`, `med`, `high`\n", .{});
     print("\n", .{});
     print("Output:\n", .{});
     print("\tLog-2:", .{});
@@ -156,4 +181,18 @@ pub fn help() void {
     print("\tLag in Frames:", .{});
     print("\tUseful for aom-av1-lavish & other forks\n", .{});
     return;
+}
+
+test "getTiles" {
+    var testrowsl: usize = 0;
+    var testcolsl: usize = 0;
+
+    var testctpx: usize = 1920 * 1080;
+    var testctar: usize = 1920 / 1080;
+    var testtpx: usize = 2000000;
+
+    _ = getTiles(testtpx, &testrowsl, &testcolsl, &testctpx, &testctar);
+
+    try testing.expect(testrowsl == 0);
+    try testing.expect(testcolsl == 0);
 }
